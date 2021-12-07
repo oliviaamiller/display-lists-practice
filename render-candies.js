e3b8ec9ad0be4e4b3fda8322c88b683ecb3d03fe
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-07 14:49:34 -0800

    added css to candies array

--- render-candies.js
+++ render-candies.js
@@ -2,7 +2,7 @@ export function renderCandies(candyData) {
     const p = document.createElement('p');
 
     p.textContent = candyData;
-    p.classList.add('candyData');
+    p.classList.add('candies');
 
     return p;
 }
