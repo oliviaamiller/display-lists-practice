ba1d8b9b8f2e3e72b13e9b154a5520e803936835
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-07 12:59:21 -0800

    rendered thrid looped array into function

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ import { renderCandies } from './render-candies.js';
 import { sweaters } from './sweaters-data.js';
 import { renderSweaters } from './render-sweaters.js';
 import { cats } from './cats-data.js';
+import { renderCats } from './render-cats.js';
 
 const candiesEl = document.querySelector('#candies-list');
-const sweatersEl = document.querySelector('#sweaters-list');const catsEl = document.querySelector('#cats-list');
+const sweatersEl = document.querySelector('#sweaters-list');
+const catsEl = document.querySelector('#cats-list');
+
 
 
 for (let candy of candies) {
@@ -22,28 +25,8 @@ for (let sweater of sweaters) {
 }
 
 for (let cat of cats) {
-    const catDiv = document.createElement('div');
-    const catNameEl = document.createElement('p');
-    const catAgeEl = document.createElement('p');
-    const catColorEl = document.createElement('p');
-    
-    const favoriteToyDiv = document.createElement('div');
-    const favoriteToyTypeEl = document.createElement('p');
-    const favoriteToyHasCatNipEl = document.createElement('p');
+    const catEl = renderCats(cat);
 
-    catDiv.classList.add('cats');
-    favoriteToyDiv.classList.add('favorite-toy');
-
-    catNameEl.textContent = cat.name;
-    catAgeEl.textContent = cat.age;
-    catColorEl.textContent = cat.color;
-    favoriteToyTypeEl.textContent = cat.favoriteToy.type;
-    favoriteToyHasCatNipEl.textContent = cat.favoriteToy.hasCatNip;
-
-    favoriteToyDiv.append(favoriteToyTypeEl, favoriteToyHasCatNipEl);
-
-    catDiv.append(catNameEl, catAgeEl, catColorEl, favoriteToyDiv);
-
-    catsEl.append(catDiv);
+    catsEl.append(catEl);
 
 }
--- render-cats.js
+++ render-cats.js
@@ -1,27 +1,26 @@
 export function renderCats(catsData) {
     const catDiv = document.createElement('div');
-        const catNameEl = document.createElement('p');
-        const catAgeEl = document.createElement('p');
-        const catColorEl = document.createElement('p');
+    const catNameEl = document.createElement('p');
+    const catAgeEl = document.createElement('p');
+    const catColorEl = document.createElement('p');
         
-        const favoriteToyDiv = document.createElement('div');
-        const favoriteToyTypeEl = document.createElement('p');
-        const favoriteToyHasCatNipEl = document.createElement('p');
+    const favoriteToyDiv = document.createElement('div');
+    const favoriteToyTypeEl = document.createElement('p');
+    const favoriteToyHasCatNipEl = document.createElement('p');
     
-        catDiv.classList.add('cats');
-        favoriteToyDiv.classList.add('favorite-toy');
+    catDiv.classList.add('cats');
+    favoriteToyDiv.classList.add('favorite-toy');
     
-        catNameEl.textContent = catsData.name;
-        catAgeEl.textContent = catsData.age;
-        catColorEl.textContent = catsData.color;
-        favoriteToyTypeEl.textContent = catsData.favoriteToy.type;
-        favoriteToyHasCatNipEl.textContent = catsData.favoriteToy.hasCatNip;
+    catNameEl.textContent = catsData.name;
+    catAgeEl.textContent = catsData.age;
+    catColorEl.textContent = catsData.color;
+    favoriteToyTypeEl.textContent = catsData.favoriteToy.type;
+    favoriteToyHasCatNipEl.textContent = catsData.favoriteToy.hasCatNip;
     
-        favoriteToyDiv.append(favoriteToyTypeEl, favoriteToyHasCatNipEl);
+    favoriteToyDiv.append(favoriteToyTypeEl, favoriteToyHasCatNipEl);
     
-        catDiv.append(catNameEl, catAgeEl, catColorEl, favoriteToyDiv);
+    catDiv.append(catNameEl, catAgeEl, catColorEl, favoriteToyDiv);
     
-        return catDiv;
+    return catDiv;
     
-    }
 }
